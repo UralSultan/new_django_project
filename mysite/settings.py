35124b31b222fc37d90057d7390c9c93eeaad2de
UralSultan <uralbeksultan@gmail.com>
2024-05-06 10:49:49 +0600

    added email sending 1 step

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'yourgmailaccount'
+EMAIL_HOST_PASSWORD = '16 буквенный пароль'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
